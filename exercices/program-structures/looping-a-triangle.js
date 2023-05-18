a32333bd486886cd8d419c0d320ee8058f8bf01d
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-05-18 16:29:49 -0300

    :books: solution: looping-a-triangle

---
+++ exercices/program-structures/looping-a-triangle.js
@@ -0,0 +1,23 @@
+/* 
+Write a loop that makes seven calls to console.log to output the following triangle:
+#
+##
+###
+####
+#####
+######
+#######
+*/
+for (let string = '#'; string.length < 8; string += '#') {
+  console.log(string);
+  // #
+  // ##
+  // ###
+  // ####
+  // #####
+  // ######
+  // #######
+}
+
+
+
